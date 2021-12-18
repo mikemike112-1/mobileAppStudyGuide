898352e25bcc781415f7e7ba86a57b72059b8d72
unknown <mmhond1@students.towson.edu>
2021-12-18 16:16:45 -0500

    initial bOne commit

---
+++ app/src/main/java/edu/towson/cosc435/mhonda/finalstudyguide/ui/pageone/PageOneView.kt
@@ -0,0 +1,13 @@
+package edu.towson.cosc435.mhonda.finalstudyguide.ui.pageone
+
+import androidx.compose.runtime.Composable
+import edu.towson.cosc435.mhonda.finalstudyguide.ui.pageone.PageOneViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun PageOneView(
+    vm: PageOneViewModel = viewModel()
+){
+
+}
--- app/src/main/java/edu/towson/cosc435/mhonda/finalstudyguide/ui/pageone/PageOneViewModel.kt
+++ app/src/main/java/edu/towson/cosc435/mhonda/finalstudyguide/ui/pageone/PageOneViewModel.kt
@@ -1,5 +1,23 @@
 package edu.towson.cosc435.mhonda.finalstudyguide.ui.pageone
 
-class PageOneViewModel {
-    private val private_name
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import edu.towson.cosc435.mhonda.finalstudyguide.model.ObjectOne
+
+class PageOneViewModel: ViewModel() {
+    private val private_name:MutableState<String> = mutableStateOf("")
+    val name: State<String> = private_name
+
+    fun setName(name: String){
+        private_name.value = name
+    }
+
+    fun validate(): ObjectOne {
+        if(name.value.isEmpty()){
+            throw Exception("error: name is empty")
+        }
+        return ObjectOne(name.value)
+    }
 }
