3b20243fa6dd801e270a6bcbbc6372470fdb40d8
unknown <mmhond1@students.towson.edu>
2021-12-18 17:57:08 -0500

    added textfield

--- app/src/main/java/edu/towson/cosc435/mhonda/finalstudyguide/MainActivity.kt
+++ app/src/main/java/edu/towson/cosc435/mhonda/finalstudyguide/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import edu.towson.cosc435.mhonda.finalstudyguide.ui.MainScreen
 import edu.towson.cosc435.mhonda.finalstudyguide.ui.theme.FinalStudyGuideTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,22 +18,9 @@ class MainActivity : ComponentActivity() {
             FinalStudyGuideTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FinalStudyGuideTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/edu/towson/cosc435/mhonda/finalstudyguide/ui/pageone/PageOneView.kt
+++ app/src/main/java/edu/towson/cosc435/mhonda/finalstudyguide/ui/pageone/PageOneView.kt
@@ -2,6 +2,7 @@ package edu.towson.cosc435.mhonda.finalstudyguide.ui.pageone
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -17,8 +18,20 @@ fun PageOneView(
         modifier = Modifier
             .fillMaxWidth()
             ) {
+
+        Text(
+            text = "plain static text here"
+        )
         Text(
             text = vm.name.value
+        )
+
+        OutlinedTextField(
+            value = vm.name.value,
+            onValueChange = vm::setName,
+            placeholder = {
+                Text(text = "name placeholder")
+            }
         )
     }
 }
