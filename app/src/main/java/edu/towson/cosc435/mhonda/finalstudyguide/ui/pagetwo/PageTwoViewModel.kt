5b1a7fe7d0ec7110daebffb7f2cc4abc3cb1855b
unknown <mmhond1@students.towson.edu>
2021-12-19 12:24:05 -0500

    before changing name of ObjectOne

--- app/src/main/java/edu/towson/cosc435/mhonda/finalstudyguide/ui/pagetwo/PageTwoViewModel.kt
+++ app/src/main/java/edu/towson/cosc435/mhonda/finalstudyguide/ui/pagetwo/PageTwoViewModel.kt
@@ -10,14 +10,24 @@ class PageTwoViewModel: ViewModel() {
     private val privateName: MutableState<String> = mutableStateOf("")
     val name: State<String> = privateName
 
+    private val privateId: MutableState<Int> = mutableStateOf(0)
+    val id: State<Int> = privateId
+
     fun setName(name: String){
         privateName.value = name
+    }
+
+    fun setId(id: Int){
+        privateId.value = id
     }
 
     fun validate(): ObjectOne {
         if(name.value.isEmpty()){
             throw Exception("error: name is empty")
         }
-        return ObjectOne(name.value)
+
+        //do i need to check the id value here?
+
+        return ObjectOne(id.value, name.value)
     }
 }
